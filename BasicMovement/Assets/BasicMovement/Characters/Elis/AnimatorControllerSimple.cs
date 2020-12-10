420e54ee4bcc5500beb77a84fe5ca9f1067a992f
KittyChaosPlanet <dokonalychaos@gmail.com>
2020-12-10 13:39:40 +0100

    Adddddddddddddd

---
+++ BasicMovement/Assets/BasicMovement/Characters/Elis/AnimatorControllerSimple.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorControllerSimple : MonoBehaviour
+{
+    Animator animator;
+    
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        bool isWalking = animator.GetBool("isWalking");
+        bool isRunning = animator.GetBool("isRunning");
+
+        bool forwardPressed = Input.GetKey(KeyCode.W);
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
+
+        // walk
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        // run
+        if (!isRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool("isRunning", true);
+        }
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+
+
+
+
+
+
+    }
+}

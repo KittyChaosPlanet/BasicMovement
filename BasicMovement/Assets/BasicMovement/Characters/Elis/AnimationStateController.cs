980383f2305de361f892a68ab95fc06aa6667ee9
KittyChaosPlanet <dokonalychaos@gmail.com>
2020-12-10 10:23:52 +0100

    adding something
    adding something

---
+++ BasicMovement/Assets/BasicMovement/Characters/Elis/AnimationStateController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+    void Update()
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+        Debug.Log(isWalking);
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+        // WALK
+     if (!isWalking && forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }  
+     if (isWalking && !forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+     // RUN
+        if (!isRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+
+
+    }
+
+    
+
+}
---
+++ BasicMovement/Assets/BasicMovement/Characters/Elis/TwoDimensionalAnimationStateController.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoDimensionalAnimationStateController : MonoBehaviour
+{
+    Animator animator;
+    float velocityZ = 0.0f;
+    float velocityX = 0.0f;
+    public float acceleration = 2.0f;
+    public float deceleration = 2.0f;
+    public float maximumWalkVelocity = 0.5f;
+    public float maximumRunVelocity = 2.0f;
+
+    //increase performance
+    int VelocityZHash;
+    int VelocityXHash;
+
+    void Start()
+    {
+        //searcg animator game object
+        animator = GetComponent<Animator>();
+
+        //increase performance
+        VelocityZHash = Animator.StringToHash("Velocity Z");
+        VelocityXHash = Animator.StringToHash("Velocity X");
+
+    }
+
+    void Update()
+    {
+        bool forwardPressed = Input.GetKey(KeyCode.W);
+        bool leftPressed = Input.GetKey(KeyCode.A);
+        bool rightPressed = Input.GetKey(KeyCode.D);
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
+        //set current velocity
+        float currentMaxVelocity = runPressed ? maximumRunVelocity : maximumWalkVelocity;
+
+        //if player presses forward, increase velocity in z direction
+        if (forwardPressed && velocityZ < currentMaxVelocity)
+        {
+            velocityZ += Time.deltaTime * acceleration;
+        }
+        //increase vel in left 
+        if (leftPressed && velocityX > -currentMaxVelocity)
+        {
+            velocityX -= Time.deltaTime * acceleration;
+        }
+        //increase vel in right
+        if (rightPressed && velocityX < currentMaxVelocity)
+        {
+            velocityX += Time.deltaTime * acceleration;
+        }
+        // decrease Z
+        if (!forwardPressed && velocityZ > 0.0f)
+        {
+            velocityZ -= Time.deltaTime * deceleration;
+        }
+        // reset vel z
+        if (!forwardPressed && velocityZ < 0.0f)
+        {
+            velocityZ = 0.0f;
+        }
+
+        // increaes vel x if left is not pressed and vel x < 0
+        if (!leftPressed && velocityX < 0.0f)
+        {
+            velocityX += Time.deltaTime * deceleration;
+        }
+
+        // increaes vel x if right is not pressed and vel x > 0
+        if (!rightPressed && velocityX > 0.0f)
+        {
+            velocityX -= Time.deltaTime * deceleration;
+        }
+
+        //reset vel x
+        if (!leftPressed && !rightPressed && velocityX != 0.0f && (velocityX > -0.05f && velocityX < 0.05f))
+        {
+            velocityX = 0.0f;
+        }
+
+
+        //lock forward 
+        if (forwardPressed && runPressed && velocityZ > currentMaxVelocity)
+        {
+            velocityZ = currentMaxVelocity;
+        }
+
+        //deccelerato to the maximum walk velocity
+        else if (forwardPressed && velocityZ > currentMaxVelocity)
+        {
+            velocityZ -= Time.deltaTime * deceleration;
+            //round to the currentmaxvelocity within offset
+            if (velocityZ > currentMaxVelocity && velocityZ < (currentMaxVelocity + 0.05f))
+            {
+                velocityZ = currentMaxVelocity;
+            }
+           
+        }
+
+        //round tothe current max velocity if without offset
+        else if (forwardPressed && velocityZ < currentMaxVelocity && velocityZ > (currentMaxVelocity - 0.05f))
+        {
+            velocityZ = currentMaxVelocity;
+        }
+
+
+        //locking left
+        if (leftPressed && runPressed && velocityX < -currentMaxVelocity)
+        {
+            velocityX = -currentMaxVelocity;
+        }
+
+        //deccelerate to the maximum walk vel
+        else if (leftPressed && velocityX < -currentMaxVelocity)
+        {
+            velocityX += Time.deltaTime * deceleration;
+            if (velocityX > currentMaxVelocity && velocityX < (currentMaxVelocity + 0.05))
+            {
+                velocityX = currentMaxVelocity;
+            }
+        }
+
+        // round to the currentmaxvelocity if within offset
+        else if (rightPressed && velocityX < currentMaxVelocity && velocityX > (currentMaxVelocity - 0.05f))
+        {
+            velocityX = currentMaxVelocity;
+        }
+
+      
+      
+
+        //set parameters to animator
+        animator.SetFloat(VelocityZHash, velocityZ);
+        animator.SetFloat(VelocityXHash, velocityX);
+
+
+    }
+}
